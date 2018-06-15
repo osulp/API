1e3b941ba7cecd75313a6b6812b43db28625cb9f
luisgreg99 <luisram90_101@hotmail.com>
2018-06-15 11:15:10 -0700

    updated routes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,20 +1,17 @@
 class ApiController < ApplicationController
+  before_action :set_params, only: [:hours]
+
   def hours
-    day = params[:day].nil? : Date.today : params[:day]
-    response_from_api = "" #Call API here to fetch hours with day
-    json = "" #HoursXmlToJsonParser.call(reponse_from_api)
-    respond_to do |format|
-      format.json { render json: json }
-    end
+    dates = @dates.nil? ? Date.today : @dates
+
+    alma = Alma.new(day, day)
+    hours = Api::HoursXmlToJsonParser.call(alma.xml_document)
+
+    render json: hours
   end
 
-  def multi_day_hours
-    beginning_day = params[:beginning_day].nil? : Date.today : params[:beginning_day] 
-    ending_day = params[:ending_day].nil? : Date.today : params[:ending_day] 
-    response_from_api = "" #Call API here to fetch hours with beginning_day and ending_day
-    json = "" #HoursXmlToJsonParser.call(reponse_from_api)
-    respond_to do |format|
-      format.json { render json: json }
-    end
+  def set_params
+    @dates = params[:dates]
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "/hours", to: "Api#hours", as: "hours"
-  get "/hours(/:day)", to: "Api#hours", as: "hours_for_day"
-  get "/multi_day_hours/:beginning_day/:end_day", to: "Api#multi_day_hours", as: "hours_for_day"
+  get "/hours(/:dates)", to: "Api#hours", as: "hours"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
