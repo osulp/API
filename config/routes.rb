5825b6ed4f37eb3ba94f21e8e74602db3e655bf1
luisgreg99 <gregorio.luisramirez@oregonstate.edu>
2018-06-22 08:41:54 -0700

    added calendar widget (#38)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   post '/hours', :to => 'api#hours', :as => 'api_hours'
-  get '/widgets/:template', :to => 'widgets#show'
+  get '/widgets/hours/:template', :to => 'widgets#hours'
 end
--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe WidgetsController, type: :controller do
-  describe "GET #show" do
+  describe "GET #hours" do
     let(:day) { }
     let(:valid_json) { File.read("spec/fixtures/alma_open_hours.json") }
-    let(:valid_xml) { "<days><day><date>2018-06-03Z</date><from>00:00</from><to>23:59</to></day></days>" }
+    let(:valid_xml) { File.read("spec/fixtures/alma_open_hours.xml") }
     let(:url) { "#{ENV['ALMA_OPEN_HOURS_URL']}?apikey=#{ENV['ALMA_API_KEY']}&from=#{date_from}&to=#{date_to}" }
-    let(:xml) { File.read("spec/fixtures/alma_open_hours.xml") }
     let(:date_from) { "2018-06-03" }
     let(:date_to) { "2018-06-09" }
     let(:cached_minutes) { "1" }
@@ -25,9 +24,15 @@ RSpec.describe WidgetsController, type: :controller do
       end
 
       it "responds to js" do
-        get :show, params: { template: 'this_weeks_hours', format: :js }
-        expect(assigns(:hours)).to include("2018-06-03")
-        expect(assigns(:hours)).to include("2018-06-09")
+        post :hours, params: { template: 'this_weeks_hours', format: :js }
+        expect(assigns(:hours)).to include(date_from)
+        expect(assigns(:hours)).to include(date_to)
+        expect(response.body).to include("This Week's Hours")
+      end
+
+      it "responds to html" do
+        get :hours, params: { template: 'calendar', format: :html }
+        expect(response.body).to include("widget_datepicker")
       end
     end
   end
