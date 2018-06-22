1a6bc433bd1bdce27007f002f45316d673ae8896
luisgreg99 <luisram90_101@hotmail.com>
2018-06-22 16:32:40 -0700

    updated specs

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -7,6 +7,7 @@ describe ApiController, type: :controller do
     let(:valid_xml) { "<days><day><date>2018-06-03Z</date><from>00:00</from><to>23:59</to></day></days>" }
     let(:alma) { Alma.new(date_from, date_to) }
     let(:url) { "#{ENV['ALMA_OPEN_HOURS_URL']}?apikey=#{ENV['ALMA_API_KEY']}&from=#{date_from}&to=#{date_to}" }
+    let(:special_hours_url) { "#{ENV['ALMA_SPECIAL_HOURS_URL']}?apikey=#{ENV['ALMA_API_KEY']}" }
     let(:xml) { File.read("spec/fixtures/alma_open_hours.xml") }
     let(:date_from) { Date.today.strftime("%Y-%m-%d") }
     let(:date_to) { Date.today.strftime("%Y-%m-%d") }
@@ -14,10 +15,20 @@ describe ApiController, type: :controller do
 
     before do
       ENV['ALMA_OPEN_HOURS_URL'] = 'https://url/to/alma/api'
+      ENV['ALMA_SPECIAL_HOURS_URL'] = 'https://url/to/alma/special/hours/api'
       ENV['ALMA_API_KEY'] = 'almaapikey123'
       ENV['ALMA_CACHED_FOR'] = '720'
 
       stub_request(:get, url).
+        with(
+            headers: {
+                'Accept'=>'*/*',
+                'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                'User-Agent'=>'Ruby'
+            }).
+        to_return(status: 200, body: "", headers: {})
+
+      stub_request(:get, special_hours_url).
         with(
             headers: {
                 'Accept'=>'*/*',
--- spec/models/alma_special_hours_spec.rb
+++ spec/models/alma_special_hours_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe AlmaSpecialHours do
-  let(:url) { "#{ENV['ALMA_SPECIAL_HOURS_URL']}?apikey=#{apikey}" }
-  let(:xml) { File.read("spec/fixtures/alma_special_hours.xml") }
+  let(:special_hours_url_base) { "#{ENV['ALMA_SPECIAL_HOURS_URL']}" }
+  let(:special_hours_url) { "#{ENV['ALMA_SPECIAL_HOURS_URL']}?apikey=#{apikey}" }
+  let(:xml_special) { File.read("spec/fixtures/alma_special_hours.xml") }
   let(:cached_for) { "1" }
   let(:apikey) { ENV['ALMA_API_KEY'] }
 
   before do
+    ENV['ALMA_OPEN_HOURS_URL'] = 'https://url/to/alma/api'
     ENV['ALMA_SPECIAL_HOURS_URL'] = 'https://url/to/alma/special/hours/api'
     ENV['ALMA_API_KEY'] = 'almaapikey123'
-    ENV['ALMA_CACHED_FOR'] = '720'
+    ENV['ALMA_CACHED_FOR'] = '1'
   end
 
   it 'fetches the xml' do
-    stub_request(:get, url).
+    stub_request(:get, special_hours_url).
         with(
             headers: {
                 'Accept'=>'*/*',
                 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
                 'User-Agent'=>'Ruby'
             }).
-        to_return(status: 200, body: xml, headers: {})
+        to_return(status: 200, body: xml_special, headers: {})
 
-    expect(described_class.fetch(url, apikey, cached_for)).to be_truthy
+    expect(described_class.fetch(special_hours_url_base, apikey, cached_for)).to be_truthy
   end
 end
