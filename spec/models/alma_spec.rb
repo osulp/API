017dd3edf7e9ae86f41e45f34397cf1db693143b
luisgreg99 <luisram90_101@hotmail.com>
2018-06-25 07:44:49 -0700

    updated alma spec

--- spec/models/alma_spec.rb
+++ spec/models/alma_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Alma do
   let(:alma) { described_class.new(date_from, date_to) }
   let(:url) { "#{ENV['ALMA_OPEN_HOURS_URL']}?apikey=#{ENV['ALMA_API_KEY']}&from=#{date_from}&to=#{date_to}" }
-  let(:special_hours_url) { "#{ENV['ALMA_SPECIAL_HOURS_URL']}?apikey=#{ENV['ALMA_API_KEY']}" }
   let(:xml) { File.read("spec/fixtures/alma_open_hours.xml") }
   let(:date_from) { "2018-06-03" }
   let(:date_to) { "2018-06-09" }
@@ -11,7 +10,6 @@ RSpec.describe Alma do
 
   before do
     ENV['ALMA_OPEN_HOURS_URL'] = 'https://url/to/alma/api'
-    ENV['ALMA_SPECIAL_HOURS_URL'] = 'https://url/to/alma/special/hours/api'
     ENV['ALMA_API_KEY'] = 'almaapikey123'
     ENV['ALMA_CACHED_FOR'] = '720'
 
@@ -23,16 +21,6 @@ RSpec.describe Alma do
                 'User-Agent'=>'Ruby'
             }).
         to_return(status: 200, body: xml, headers: {})
-
-
-    stub_request(:get, special_hours_url).
-        with(
-            headers: {
-                'Accept'=>'*/*',
-                'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                'User-Agent'=>'Ruby'
-            }).
-        to_return(status: 200, body: "", headers: {})
   end
 
   it 'sets a hash variable' do
