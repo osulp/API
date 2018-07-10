dbb0ab592014bc65929d4277583949ca2dcfea5b
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2018-07-10 16:11:49 -0700

    update special hours widget

--- app/services/api/special_hours_xml_to_json_parser.rb
+++ app/services/api/special_hours_xml_to_json_parser.rb
@@ -22,11 +22,44 @@ module API
             from_hour: parse_child(day.xpath("from_hour")),
             to_hour: parse_child(day.xpath("to_hour")),
             day_of_week: parse_child(day.xpath("day_of_week")),
-            status: parse_child(day.xpath("status"))
+            status: parse_child(day.xpath("status")),
+            formatted_hours: get_formatted_hours(day),
+            formatted_dates: get_formatted_dates(day)
         }
         json_data[i.to_s] = data
       end
       json_data.to_json
+    end
+
+    def self.get_formatted_dates(day)
+      from_date = parse_child(day.xpath("from_date"))
+      to_date = parse_child(day.xpath("to_date"))
+
+      if from_date.present? && to_date.present? && from_date != to_date
+        from_date_str = DateTime.parse(from_date.to_s).strftime("%m/%d/%Y")
+        to_date_str = DateTime.parse(to_date.to_s).strftime("%m/%d/%Y")
+        "#{from_date_str} - #{to_date_str}"
+      elsif from_date.present? && to_date.present? && from_date == to_date
+        DateTime.parse(from_date.to_s).strftime("%m/%d/%Y")
+      elsif from_date.present? && to_date.blank?
+        DateTime.parse(from_date.to_s).strftime("%m/%d/%Y")
+      else
+       from_date
+      end
+    end
+
+    def self.get_formatted_hours(day)
+      status = parse_child(day.xpath("status"))
+      from_hour = parse_child(day.xpath("from_hour"))
+      to_hour = parse_child(day.xpath("to_hour"))
+
+      if status == "CLOSE"
+        "Closed"
+      elsif status == "OPEN" && from_hour.present? && to_hour.present?
+        "#{from_hour} - #{to_hour}"
+      else
+       status
+      end
     end
 
     def self.parse_child(xml)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'api.library.oregonstate.edu', :protocol => 'http' }
+  config.action_mailer.default_url_options = { :host => 'api.library.oregonstate.edu', :protocol => 'https' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
