8eb52ba6b2f5d1bb14d7487881fde7d80117f402
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-08-12 13:20:57 -0700

    puma configs for prod/staging

---
+++ config/puma/staging.rb
@@ -0,0 +1,15 @@
+#
+directory '/data'
+bind 'tcp://0.0.0.0:3000'
+pidfile '/tmp/puma.pid'
+state_path '/tmp/puma.state'
+workers 4
+preload_app!
+environment 'staging'
+
+daemonize false
+# Allow for `touch tmp/restart.txt` to force puma to restart the app
+#plugin :tmp_restart
+
+# Activate the puma control application, mapping location in the nginx config
+activate_control_app 'unix:///tmp/pumactl.sock'
