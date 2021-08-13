5e3b718fe2de49a5c1fbef7e4b145811055934f8
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-08-13 12:26:27 -0700

    remove daemonize for new puma version

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal:true
 #
+
 directory '/data'
 bind 'tcp://0.0.0.0:3000'
 pidfile '/tmp/puma.pid'
@@ -6,10 +8,6 @@ state_path '/tmp/puma.state'
 workers 4
 preload_app!
 environment 'production'
-
-daemonize false
-# Allow for `touch tmp/restart.txt` to force puma to restart the app
-#plugin :tmp_restart
 
 # Activate the puma control application, mapping location in the nginx config
 activate_control_app 'unix:///tmp/pumactl.sock'
