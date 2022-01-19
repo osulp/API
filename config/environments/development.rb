7994d71eda332536726662a30000bd7b03aeaaf8
Student <student@Students-MacBook-Pro.local>
2022-01-19 14:36:42 -0800

    update ruby to 2.7.1 and rails to 6.0.4.4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.hosts << "test.library.oregonstate.edu"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
